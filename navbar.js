8a25a769dc9c8ca7eb608bb7f11d2a0493336fd5
Devin Chen <taunwe79@gmail.com>
2025-08-14 10:46:23 -0500

    added navjar.html and navbar.js to make navbar appear on each page and so that I don't have to change the code on each page. Dynamic changes. Also added a back to blog page and back to card class navigational back button to each article/card class.

---
+++ navbar.js
@@ -0,0 +1,8 @@
+// Javascript script to automate fetching of the navbar.
+
+fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading navbar:", error));
